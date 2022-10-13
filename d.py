10ac4c8df0908240c42292de1943a8750c1f41d5
minsu <splendent77@gmail.com>
2022-10-13 21:08:02 +0900

    Update : 여러명의 사람 각자의 사진

--- d.py
+++ d.py
@@ -12,17 +12,21 @@ result = [item for item in result if item[6]=='person']
 print(result)
 print(results.xyxy[0][0][0].item())
 
-### 1번 ###
+### 2번 ### cv.imwrite에서 f스트링을 안쓰니까 사진이 한장이 나왔다.
+#모든 사람의 crop된 사진이 나왔지만 이미 rectangle이 된 사진으로 crop을 해서 사각형들이 보인다.
 
 
 
-for a,b in enumerate(result):
-    tmp_img = cv2.imread('test.jpg')
-    print(tmp_img.shape)
-    cropped = tmp_img[int(a[0][1]):int(a[0][3]), int(a[0][0]):int(a[0][2])]
-    print(cropped.shape)
-    cv2.rectangle(tmp_img, (int([b][0][0].item()), int([b][0][1].item())), (int(results.xyxy[b][0][2].item()), int(results.xyxy[b][0][3].item())), (255,255,255))
-    cv2.imwrite('result.png', tmp_img)
+tmp_img = cv2.imread('test.jpg')
+for a in (result):
+    cropped = tmp_img[int(a[1]):int(a[3]), int(a[0]):int(a[2])]
+    cv2.imwrite('result.png', cropped)
+    cv2.rectangle(tmp_img, (int(a[0]), int(a[1])), (int(a[2]), int(a[3])), (0,255,255))
+
+
+    cropped = tmp_img[int(a[1]):int(a[3]), int(a[0]):int(a[2])]
+    cv2.imwrite(f"{a}.png", cropped)
+cv2.imwrite('result.png', tmp_img)
 
 
 
